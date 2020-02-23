48cf876fddd0a9f97480c2ab7123626e58bd7876
sho19 <areeb2132@gmail.com>
2020-02-23 22:03:03 +0530

    implemented firebase utils, included ability for users to store authenticated users into firebase database

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./pages/homepage/homepage.styles.scss"
 import Header from "./components/header/header.component";
 import Shop from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {auth} from "./firebase/firebase-utils"
+import {auth, createUserProfileDocument } from "./firebase/firebase-utils"
 
 class App extends Component {
     constructor(){
@@ -19,10 +19,24 @@ class App extends Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth=auth.onAuthStateChanged(user=>{
-            this.setState({currentUser:user});
-            console.log(user);
-        })
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    });
+
+                    console.log(this.state);
+                });
+            }
+
+            this.setState({ currentUser: userAuth });
+        });
     }
 
     componentWillUnmount() {
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,16 +2,23 @@ import React,{Component} from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import {signInWithGoogle} from "../../firebase/firebase-utils";
+import {auth, signInWithGoogle} from "../../firebase/firebase-utils";
 class SignIn extends Component{
     constructor(){
         super();
         this.state = {email:"",password:""};
     }
 
-    handleSubmit = event =>{
+    handleSubmit = async event =>{
       event.preventDefault();
-      this.setState({email:"",password:""});
+      const {email,password} = this.state;
+      try {
+          await auth.signInWithEmailAndPassword(email,password);
+          this.setState({email:"",password:""});
+      }
+      catch (e) {
+        console.log(e)
+      }
     };
 
     handleChange = event =>{
